04059248678d1cc3bf1683b2a74d5c5f8d54d318
Aasif Shaikh <acloudiator@gmail.com>
2017-05-04 10:42:40 -0700

    Initial Commit with build directory

---
+++ bin/terraform-provider-linode/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/btobolaski/terraform-provider-linode"
+	"github.com/hashicorp/terraform/plugin"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: linode.Provider,
+	})
+}

277f4867427b91381055f80e6b3a4c611c1b5918
Aasif Shaikh <acloudiator@gmail.com>
2017-05-18 14:57:34 -0700

    Fix to include the host IP which was missing
    Also included the latest plugin under bin

--- bin/terraform-provider-linode/main.go
+++ bin/terraform-provider-linode/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/btobolaski/terraform-provider-linode"
+	"github.com/hashicorp/terraform/builtin/providers/linode"
 	"github.com/hashicorp/terraform/plugin"
 )
 
